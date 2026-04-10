9c4b2898de4fbc5f7210887f6cc21273fcf12eda
Leo Zhou <128760280+laoz40@users.noreply.github.com>
2026-04-11 01:36:20 +1000

    style: fix spacing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -273,7 +273,7 @@
 	onDeleteEntry={deleteHistoryEntry}
 />
 
-<div class="flex h-screen flex-col gap-6 overflow-hidden px-6 py-8">
+<div class="flex h-screen flex-col gap-4 overflow-hidden px-6 py-8">
 	<h1 class="self-center text-6xl font-bold text-primary">TTT</h1>
 
 	<section class="flex flex-row items-center gap-2 self-center">
